3e4b1715354a739a0cd864f5f587214533fd40a2
brmsuser <>
2018-04-30 13:48:04 -0300

    {/testProject/src/main/java/com/testspace/testproject/Address.java}

--- testProject/src/main/java/com/testspace/testproject/Address.java
+++ testProject/src/main/java/com/testspace/testproject/Address.java
@@ -6,12 +6,24 @@ package com.testspace.testproject;
 
 public class Address implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public Address() {
-    }
+	@org.kie.api.definition.type.Label(value = "name")
+	private java.lang.String name;
 
+	public Address() {
+	}
 
+	public java.lang.String getName() {
+		return this.name;
+	}
 
+	public void setName(java.lang.String name) {
+		this.name = name;
+	}
+
+	public Address(java.lang.String name) {
+		this.name = name;
+	}
 
 }
